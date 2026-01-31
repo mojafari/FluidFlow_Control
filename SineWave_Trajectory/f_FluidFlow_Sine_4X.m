39f26aad32ff7aa7f1b71d02291b5d906fca9a6c
mojafari <mohamadj@gmail.com>
2026-01-31 17:22:25 -0500

    Add files via upload

---
+++ SineWave_Trajectory/f_FluidFlow_Sine_4X.m
@@ -0,0 +1,96 @@
+function dx = f_FluidFlow_Sine_4X(t,s)
+%% Reference trajectory
+r = [11 10 7 4]' + 1*cos(([2 3 4 5]').*t).*[10 5 2 1]';
+rd = -([2 3 4 5]').*sin(([2 3 4 5]').*t).*[10 5 2 1]';
+
+%% disturbance
+d_1 = 5;    % actual disturbance
+d_2 = 4;    % actual disturbance
+d_3 = 3;    % actual disturbance
+d_4 = 2;    % actual disturbance
+d = [d_1 d_2 d_3 d_4]';
+
+%% params
+L1 = 557.7;
+L2 = 1016.9;
+L3 = 41.0;
+L4 = -628.9;
+
+M11 = -86.1;
+M22 = -392.4;
+M23 = 263.9;
+M32 = -218.3;
+M33 = -7.6;
+M41 = 43.4;
+M44 = -113.5;
+
+N111 = 1.8;
+N121 = -2.2;
+N131 = -2.3;
+N141 = -6.8;
+N212 = 75.0;
+N313 = 5.0;
+N314 = 3.9;
+N414 = 2.9;
+N424 = -9.8;
+N434 = 6.3;
+N444 = -7.3;
+
+O2222 = -2.5;
+O2233 = -2.5;
+O3223 = -0.2;
+O3333 = -0.2;
+
+%% States assignment
+x1 = s(1);
+x2 = s(2);
+x3 = s(3);
+x4 = s(4);
+
+k = s(5);
+
+dhx1 = s(6);
+dhx2 = s(7);
+dhx3 = s(8);
+dhx4 = s(9);
+
+X = [x1 x2 x3 x4]';
+
+dh = [dhx1 dhx2 dhx3 dhx4]';
+
+%% control gains and learning rate of gradient descent
+alpha = 10*1.50; %50;
+alphadh = 10*1.10; %10;
+
+kd = 10*1.20; %2;
+
+
+%% Definition of Fluid Flow parameters
+
+dx1 = L1 + M11*x1 + N111*x1^2 + N121*x1*x2 + N131*x1*x3 + N141*x1*x4;
+dx2 = L2 + M22*x2 + M23*x3 + N212*x1*x2 + O2222*x2^3 + O2233*x2*x3^2;
+dx3 = L3 + M32*x2 + M33*x3 + N313*x1*x3 + N314*x1*x4 + O3223*x3*x2^2 + O3333*x3^3;
+dx4 = L4 + M41*x1 + M44*x4 + N414*x1*x4 + N424*x2*x4 + N434*x3*x4 + N444*x4^2;
+
+
+F = [dx1;dx2;dx3;dx4];
+B = eye(4);
+
+%% cost definitions
+e = X - r;
+ed = -k*eye(4)*e + d - dh;
+
+c = ed + kd*eye(4)*e;
+
+%% First order ODEs
+dk = alpha*c'*e;
+ddh = alphadh*c;
+
+u = inv(B)*(-F - k*eye(4)*e + rd -dh);
+
+fileID = fopen('exp_sine_4X.txt','a');
+fprintf(fileID,'%f %f %f\n',[t;u(1);u(2);u(3);u(4)]);
+fclose(fileID);
+dx = [F+B*u+d;dk;ddh];
+
+end
